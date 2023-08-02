93d973bc09b5849edf1f4c4b88a151f0517216fb
uc-sag <sagar.gupta@ucertify.com>
2023-08-02 15:24:36 +0530

    Third

--- index1.php
+++ index1.php
@@ -1,9 +1,17 @@
-<?php
- require_once('C:\smarty-4.3.1\libs/Smarty.class.php');
- $smarty = new Smarty();
 
- $smarty->assign("test","'Hello this sagar'");
- $smarty->assign("mail",'sagar.gupta@ucertify.com');
-$smarty->display('first.tpl');
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+       for($i=1;$i<=10;$i++){
+          echo "5 X $i =", 5*$i;
+          echo "<br>";
+       }
+    ?>
+</body>
+</html>
